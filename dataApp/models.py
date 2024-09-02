27459c3e13f2dbe16cc9994885e844def0170957
root <root@DESKTOP-G40S1JC.localdomain>
2024-09-02 12:40:32 -0700

    user data collection app

--- dataApp/models.py
+++ dataApp/models.py
@@ -5,6 +5,12 @@ class UserDetails(models.Model):
     age = models.IntegerField()
     occupation = models.CharField(max_length=200)
     location = models.CharField(max_length=255, default="ee")  # Add a default value (empty string)
+    email = models.EmailField()
+    file = models.FileField(upload_to='documents/')
+    saved_time = models.DateTimeField(auto_now_add=True)
+
+
+
 
     def __str__(self):
         return self.name
